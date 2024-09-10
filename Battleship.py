1d9fe90d52aa0923416ef09eb274052c712e7183
Timo Alejandro Aranjo <alex.aranjo@gmail.com>
2024-09-10 09:59:29 -0500

    Update Battleship.py
    Fixed spaces

--- Battleship.py
+++ Battleship.py
@@ -1,5 +1,5 @@
 class Player:
-	def __init__(self):
+    def __init__(self):
         #Board 1 and 2 will need to be initialized here
 
         #Sink, hit, and over variables update every turn
@@ -19,36 +19,36 @@ class Player:
 		
     #Sets a coordinate on the board to a specific value
     #Point must be valid, no parameter checking should occur here
-	def add_Point(self, board, coord, value):
+    def add_Point(self, board, coord, value):
 
     #Returns the value at a specific point on the board.
     #Point must be valid, no parameter checking should occur here
-	def get_Point(self, board, coord):
+    def get_Point(self, board, coord):
     	
     #Steps:
     #Make sure all locations the ship will exist along are valid using BOB
     #Make sure the ship doesn't overlap with any other ship
-	def add_Ship(self, board, coord, size, downright):
+    def add_Ship(self, board, coord, size, downright):
 
     #Steps:
     #Make sure point is a valid point using BOB
     #Mark location on board
     #Set hit, sink, over variables
-	def add_Shoot(self, board, coord):
+    def add_Shoot(self, board, coord):
 	
     #Do not implement status.
     #Realized that as long as the object variables are set, there is no need
     #to return them for internal use. Just access and set variables directly.
-	def status(self):
+    def status(self):
 	
     #Returns both player's boards
     #Remember to define these in init
-	def get_Boards(self):
+    def get_Boards(self):
         return self._board_One, self._board_Two
 
 class Interface:
-	def init(self):
+    def init(self):
 	
     #This starts the entire program. The entire interface is ran from this function.
-	def start(self):
+    def start(self):
 		
