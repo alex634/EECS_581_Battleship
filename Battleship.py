975029b07a9274f59a17cf4c2a3f35992d3f0531
h334w620 <h334w620@ku.edu>
2024-09-10 14:41:17 -0500

    filled in some of the basic list manipulation functions

--- Battleship.py
+++ Battleship.py
@@ -16,14 +16,28 @@ class Player:
     #This does not check for existence of ships, just that the coordinate is valid
     #Within the board space
     def __bob(self, coord):
-		
+
+    # takes in a tuple (<uppercase letter>, <number>)
+    # returns interger repersentation of the coordinate in the XY plane
+    def __str_coord_to_xy(self, coord): 
+        # ord returns the interger repersentation of the ascii character
+        # 64 is the offset for the A character
+        x = ord(coord[0]) - 64
+        y = int(coord[1])
+
+        return x,y
+        
     #Sets a coordinate on the board to a specific value
     #Point must be valid, no parameter checking should occur here
     def __add_Point(self, board, coord, value):
-
+        x,y = self.__str_coord_to_xy(coord)
+        board[y][x] = value
+        
     #Returns the value at a specific point on the board.
     #Point must be valid, no parameter checking should occur here
     def __get_Point(self, board, coord):
+        x,y = self.__str_coord_to_xy(coord)
+        return board[y][x]
     	
     #Steps:
     #Make sure all locations the ship will exist along are valid using BOB
@@ -47,8 +61,18 @@ class Player:
         return self._board_One, self._board_Two
 
 class Interface:
-    def init(self):
-	
+    def __init__(self):
+
+    # prints out the game board list for debug purpose
+    def __debug_show_list(self):
+
+    # print out the game board for players
+    def __show_board(self):
+
+    # print out the hidden game board for players
+    def __hidden_board(self):
+        
+
     #This starts the entire program. The entire interface is ran from this function.
     def start(self):
 		
